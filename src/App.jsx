329cca1af6f37b261f240a05beceffbb5de7af4e
MD Sharik <md.sharik5012@gmail.com>
2024-05-23 13:39:18 +0530

    s

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Spline from "@splinetool/react-spline";
+function App() {
+  return (
+    <>
+      <Spline
+        style={{ height: "100vh" }}
+        scene="https://prod.spline.design/GDECfwhDMfcnD6nG/scene.splinecode"
+      />
+    </>
+  );
+}
+
+export default App;
